41c302e733367c4935171157b2071a2ec689f68d
Pablo Rufat <pablorufatdev@gmail.com>
2021-09-16 11:29:33 -0300

    feat(routes): Create user routes.

---
+++ src/api/route/user.routes.js
@@ -0,0 +1,10 @@
+import * as express from "express";
+import UserController from "../controller";
+
+const router = express.Router();
+
+router.post('/users', UserController.signUp);
+router.post('/users/admin', UserController.signUpAdmin);
+router.post('/login', UserController.signIn);
+
+export default router;
