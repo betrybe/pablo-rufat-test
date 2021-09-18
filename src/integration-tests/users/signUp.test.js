efc4e230df3c8ce2fea4130aa6b2f1cdfa9cd343
Pablo Rufat <pablorufatdev@gmail.com>
2021-09-18 16:38:29 -0300

    test(signUp): Implement signup endpoint tests

--- src/integration-tests/users/signUp.test.js
+++ src/integration-tests/users/signUp.test.js
@@ -1,45 +1,137 @@
 const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../../api/app');
 const { CONNECTION_ERROR, MONGO_DB_URL} = require('../../api/utils/constants');
 const { User } = require("../../api/model");
+const { Roles } = require('../../api/utils/interfaces');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
 
 describe('SignUp Endpoint tests.', () => {
-  before(async () => {
-    await mongoose.connect(MONGO_DB_URL)
-    .catch((error) => {
-        console.error(CONNECTION_ERROR, error);
-        process.exit(1);
+    before(async () => {
+        await mongoose.connect(MONGO_DB_URL)
+            .catch((error) => {
+                console.error(CONNECTION_ERROR, error);
+                process.exit(1);
+            });
     });
-  });
 
-  beforeEach(async () => {
-    await User.deleteMany();
-  });
+    beforeEach(async () => {
+        await User.deleteMany();
+    });
 
-  after(async () => {
-    await mongoose.disconnect();
-  });
+    after(async () => {
+        await mongoose.disconnect();
+    });
 
-  it('Should return error 400 when field name is missing', async () => {
+    it('Should return error 400 when field name is missing', async () => {
+        chai.request(app)
+        .post('/users')
+        .send({
+            email: "test3@test.com",
+            password: "1234567890"
+        })
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+            }
 
-  });
+            expect(res).to.have.status(400);
+        });
+    });
 
-  it('Should return error 400 when field email is missing', async () => {
+    it('Should return error 400 when field email is missing', async () => {
+        chai.request(app)
+        .post('/users')
+        .send({
+            name: "name",
+            password: "1234567890"
+        })
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+            }
 
-  });
+            expect(res).to.have.status(400);
+        });
+    });
 
-  it('Should return error 400 when field email is invalid', async () => {
+    it('Should return error 400 when field email is invalid', async () => {
+        chai.request(app)
+        .post('/users')
+        .send({
+            name: "name",
+            email: "test3",
+            password: "1234567890"
+        })
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+            }
 
-  });
+            expect(res).to.have.status(400);
+        });
+    });
 
-  it('Should return error 400 when field password is missing', async () => {
+    it('Should return error 400 when field password is missing', async () => {
+        chai.request(app)
+        .post('/users')
+        .send({
+            name: "name",
+            email: "test3@test.com",
+        })
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+            }
 
-  });
+            expect(res).to.have.status(400);
+        });
+    });
 
-  it('Should return error 409 when email already exists', async () => {
+    it('Should return error 409 when email already exists', async () => {
+        await chai.request(app)
+        .post('/users')
+        .send({
+            name: "name",
+            email: "test3@test.com",
+            password: "1234567890"
+        });
 
-  });
+        chai.request(app)
+        .post('/users')
+        .send({
+            name: "name",
+            email: "test3@test.com",
+            password: "1234567890"
+        })
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+            }
 
-  it('Should return 201 and the field role value must be "user"', async () => {
+            expect(res).to.have.status(409);
+        });
+    });
 
-  });
+    it('Should return 201 and the field role value must be "user"', async () => {
+        chai.request(app)
+        .post('/users')
+        .send({
+            name: "name",
+            email: "test3@test.com",
+            password: "1234567890"
+        })
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+            }
+
+            expect(res).to.have.status(201);
+            expect(res.body).to.have.haveOwnProperty('user');
+            expect(res.body.user.role).to.be.equal(Roles.USER);
+        });
+    });
 });
