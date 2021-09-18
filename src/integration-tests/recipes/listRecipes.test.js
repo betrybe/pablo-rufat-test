6a8d9afe66383c0ceb4136f29aab18800c8d5fc6
Pablo Rufat <pablorufatdev@gmail.com>
2021-09-18 18:46:01 -0300

    test(recipes): implement tests for listRecipes endpoint

--- src/integration-tests/recipes/listRecipes.test.js
+++ src/integration-tests/recipes/listRecipes.test.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../../api/app');
 const { CONNECTION_ERROR, MONGO_DB_URL} = require('../../api/utils/constants');
 const { User, Recipe } = require("../../api/model");
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+const request = chai.request;
+
+let token;
 
 describe('ListRecipes Endpoint tests.', () => {
   before(async () => {
@@ -9,22 +18,62 @@ describe('ListRecipes Endpoint tests.', () => {
         console.error(CONNECTION_ERROR, error);
         process.exit(1);
     });
-  });
 
-  beforeEach(async () => {
-    await User.deleteMany();
-    await Recipe.deleteMany();
+    await request(app)
+    .post('/users')
+    .send({
+        name: "name",
+        email: "test3@test.com",
+        password: "1234567890",
+    });
+
+    const loginResponse = await request(app)
+    .post('/login')
+    .send({
+        email: "test3@test.com",
+        password: "1234567890"
+    });
+
+    token = loginResponse.body.token;
+
+    await request(app)
+    .post('/recipes')
+    .set('Authorization', token)
+    .send({
+        name: "name",
+        ingredients: "ingredients",
+        preparation: "preparation",
+    });
   });
 
   after(async () => {
+    await User.deleteMany();
+    await Recipe.deleteMany();
     await mongoose.disconnect();
   });
 
   it('Should return 200 when Authorization header is missing', async () => {
+    request(app)
+    .get('/recipes')
+    .end((err, res) => {
+      if (err) {
+          console.error(err);
+      }
 
+      expect(res).to.have.status(200);
+    });
   });
 
   it('Should return 200 when Authorization header is present', async () => {
+    request(app)
+    .get('/recipes')
+    .set('Authorization', token)
+    .end((err, res) => {
+      if (err) {
+          console.error(err);
+      }
 
+      expect(res).to.have.status(200);
+    });
   });
 });
